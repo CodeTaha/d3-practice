43a42c85ab8ccfc1c3c07ac2df993338cb24ac0f
codetaha <taha.kachwala21@gmail.com>
2016-08-09 14:33:30 +0300

    some changes

--- ReactFlux/src/components/authors/manageAuthorPage.jsx
+++ ReactFlux/src/components/authors/manageAuthorPage.jsx
@@ -3,9 +3,24 @@
 var React = require("react");
 var AuthorForm = require('./authorForm.jsx');
 var ManageAuthorPage = React.createClass({
+	getInitialState: function(){
+		return {
+			author: {id:'', firstName: '', lastName:''}, 
+		};
+	},
+	setAuthorState: function(event) {
+		var field = event.target.name;
+		var value = event.target.value;
+		this.state.author[field] = value;
+		console.log(this.state.author)
+		return this.setState({author:this.state.author});
+	},
 	render: function() {
 		return (
-			<AuthorForm/>
+			<AuthorForm 
+				author={this.state.author}
+				onChange={this.setAuthorState}
+			/>
 		);
 	}
 });
